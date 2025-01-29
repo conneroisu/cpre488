45158f098c5aded0c8a8008177753b95d140a7d0
connero <88785126+conneroisu@users.noreply.github.com>
2025-01-29 09:05:24 -0600

    got a start on the first part of the report (analysis of given c code) (#3)

--- flake.nix
+++ flake.nix
@@ -88,6 +88,68 @@
                 binutils
                 alejandra
                 libclang
+                (pkgs.buildFHSEnv {
+                  name = "xilinx-env";
+                  inherit "bash";
+                  targetPkgs = pkgs:
+                    with pkgs; let
+                      ncurses' = ncurses5.overrideAttrs (old: {
+                        configureFlags = old.configureFlags ++ ["--with-termlib"];
+                        postFixup = "";
+                      });
+                    in [
+                      bash
+                      coreutils
+                      zlib
+                      lsb-release
+                      stdenv.cc.cc
+                      # https://github.com/NixOS/nixpkgs/issues/218534
+                      # postFixup would create symlinks for the non-unicode version but since it breaks
+                      # in buildFHSUserEnv, we just install both variants
+                      ncurses'
+                      (ncurses'.override {unicodeSupport = false;})
+                      xorg.libXext
+                      xorg.libX11
+                      xorg.libXrender
+                      xorg.libXtst
+                      xorg.libXi
+                      xorg.libXft
+                      xorg.libxcb
+                      xorg.libxcb
+                      # common requirements
+                      freetype
+                      fontconfig
+                      glib
+                      gtk2
+                      gtk3
+                      libxcrypt-legacy # required for Vivado
+                      python3
+
+                      (libidn.overrideAttrs (_old: {
+                        # we need libidn.so.11 but nixpkgs has libidn.so.12
+                        src = fetchurl {
+                          url = "mirror://gnu/libidn/libidn-1.34.tar.gz";
+                          sha256 = "sha256-Nxnil18vsoYF3zR5w4CvLPSrTpGeFQZSfkx2cK//bjw=";
+                        };
+                      }))
+
+                      # to compile some xilinx examples
+                      opencl-clhpp
+                      ocl-icd
+                      opencl-headers
+
+                      # from installLibs.sh
+                      graphviz
+                      (lib.hiPrio gcc)
+                      unzip
+                      nettools
+                    ];
+                  multiPkgs = ps: [];
+                  profile = ''
+                    export LC_NUMERIC="en_US.UTF-8"
+                    source /opt/xilinx/Vitis/*/settings64.sh
+                  '';
+                })
               ]
               ++ (lib.optionals isLinux (with pkgs; [
                 xorg.libX11
@@ -114,6 +176,7 @@
                     stdenv.cc
                     tbb # Intel Threading Building Blocks
                     llvmPackages.openmp # OpenMP support
+                    tcl
                   ])
                   ++ (lib.optionals isLinux [
                     pkgs.xorg.libX11
@@ -144,6 +207,9 @@
                 enable = true;
               };
               c = {
+                enable = true;
+              };
+              cplusplus = {
                 enable = true;
               };
               zig = {
