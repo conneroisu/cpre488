9b50d50d2a703aa11286ab5b536a732af72a3ad2
Nolan <nolieman1@gmail.com>
2025-02-07 17:03:34 -0600

    -Created a basic environment for developing VHDL -Currently working on a design to make the blank bits of the pixel data be the MSBs not the LSBs.

---
+++ mps/MP-0/VHDL/work/pixel_stream_correction.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+library work;
+
+use IEEE.std_logic_1164.all;
+use work.user_types.all;
+
+entity pixel_stream_correction is
+    port(
+            i_data_stream : in STD_LOGIC_VECTOR(STREAM_SIZE - 1 downto 0);
+            o_corrected_data_stream : out STD_LOGIC_VECTOR(15 downto 0)
+        );
+end pixel_stream_correction;
+
+architecture rtl of pixel_stream_correction is
+begin
+    o_corrected_data_stream <= (B"00" & i_data_stream(i_data_stream'high downto 2));
+end architecture;
---
+++ mps/MP-0/VHDL/work/pixel_stream_correction_tb.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use work.user_types.all;
+
+entity pixel_stream_correction_tb is
+end pixel_stream_correction_tb;
+
+architecture sim of pixel_stream_correction_tb is
+    signal s_in_data_stream, s_out_corrected_data_stream : STD_LOGIC_VECTOR(STREAM_SIZE - 1 downto 0);
+begin
+
+    corrector : entity work.pixel_stream_correction(rtl)
+    port map(
+                i_data_stream => s_in_data_stream,
+                o_corrected_data_stream => s_out_corrected_data_stream
+            );
+
+    process
+    begin
+        -- Create data
+        s_in_data_stream <= X"ABC0";
+        wait for 10 ns;
+        report "Finished!";
+        wait;
+    end process;
+
+end architecture;
