987c84b52ad8371be3069974875399d4336c68af
connero <88785126+conneroisu@users.noreply.github.com>
2025-02-12 16:44:03 -0600

    Fix my old implementation of Game Menu PPm Loading (#22)
    * fixed some of the close methods for th ppm opening method
    
    * cleaning

--- mps/MP-0/eclipse_workspace/Xentendo/src/lib/menu/menu.c
+++ mps/MP-0/eclipse_workspace/Xentendo/src/lib/menu/menu.c
@@ -42,7 +42,7 @@
 typedef uint16_t u16;
 typedef uint8_t u8;
 
-// The framebuffer is defined globally only to allocate storage.
+// The frame buffer is defined globally only to allocate storage.
 // All drawing functions now take a pointer to the framebuffer as an argument.
 u16 framebuffer[VIDEO_HEIGHT][VIDEO_WIDTH];
 
@@ -62,7 +62,7 @@ Game games[] = {
 		2025,
 		"Arcade",
 		"ISUBros.nes",
-		"foo.ppm"
+		"foo"
 	},
 
     {
@@ -70,7 +70,7 @@ Game games[] = {
         1985,                //
         "Action/Simulation", //
         "Paperboy.nes",      //
-        "Paperboy.ppm"       //
+        "Paperboy"       //
     },                       //
 
 	{
@@ -78,7 +78,7 @@ Game games[] = {
 		1988,
 		"Platformer",
 		"smario2.nes",
-		"foo.ppm"
+		"foo"
 	},
 
 	{
@@ -86,7 +86,7 @@ Game games[] = {
 		1988,
 		"Platformer",
 		"smario3.nes",
-		"foo.ppm"
+		"foo"
 	},
 
     {
@@ -94,21 +94,21 @@ Game games[] = {
         1987,                //
         "Action-Platformer", //
         "MegaMan.nes",       //
-        "MegaMan.ppm"        //
+        "MegaMan"        //
     },                       //
     {
         "The Legend of Zelda",  //
         1986,                   //
         "Action-Adventure",     //
         "zelda.nes", //
-        "TheLegendOfZelda.ppm"  //
+        "TheLegendOfZelda"  //
     },                          //
     {
         "The Legend of Zelda II", //
         1987,                     //
         "Action-Adventure/RPG",   //
         "zelda2.nes", //
-        "TheLegendOfZeldaII.ppm"  //
+        "TheLegendOfZeldaII"  //
     },
 
 	{
@@ -116,7 +116,7 @@ Game games[] = {
 		1984,
 		"Scrolling Shooter",
 		"1943.nes",
-		"foo.ppm"
+		"foo"
 	}
 
 
@@ -394,7 +394,6 @@ u8 *load_ppm(const char *filename, int *width, int *height) {
 
   xil_printf("menu.load_ppm(): beginning loading coverfile %s\r\n", filename);
   Xil_DCacheDisable();
-
   status = xilsd_fopen(fp, filename);
   if (status != XST_SUCCESS) {
     xil_printf("menu.load_ppm(): Error opening coverfile %s %h\r\n", filename,
@@ -442,15 +441,15 @@ u8 *load_ppm(const char *filename, int *width, int *height) {
   if (max_val != 255) {
     fprintf(stderr, "Warning: max color value is not 255 (%d).\n", max_val);
   }
-  fgetc(fp); // consume whitespace after header
+	fgetc(fp); // consume whitespace after header
   size_t data_size = 3 * (*width) * (*height);
   static u8 image_data[MAX_PPM_WIDTH * MAX_PPM_HEIGHT * 3];
   if (fread(image_data, 1, data_size, fp) != data_size) {
     fprintf(stderr, "Error: Could not read image data from %s\n", filename);
-    fclose(fp);
+    xilsd_fclose(fp);
     return NULL;
   }
-  fclose(fp);
+  xilsd_fclose(fp);
   Xil_DCacheEnable();
   return image_data;
 }
