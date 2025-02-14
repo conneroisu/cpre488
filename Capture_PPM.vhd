88925e2079cbb1525c42bc994167fb1c99ff293b
Parker <oparker@iastate.edu>
2025-02-14 11:46:03 -0600

    testbench

--- Capture_PPM.vhd
+++ Capture_PPM.vhd
@@ -22,6 +22,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -48,10 +49,10 @@ end Capture_PPM;
 architecture Behavioral of Capture_PPM is
 
 signal counter : integer range 0 to 15;
-signal validate_counter : interger range 0 to 15;
+signal validate_counter : integer range 0 to 15;
 signal temp_output : unsigned (31 downto 0);
 
-type state_type is (IDLE,VALIDATE,MEASURE,GAP,STORE);
+type state_type is (IDLE,VALIDATE_LOW,VALIDATE_HIGH,MEASURE,GAP,STORE);
 signal PS,NS : state_type;
 
 begin
@@ -77,22 +78,23 @@ begin
                 out_channel_5 <= (others => '0');
                 out_channel_6 <= (others => '0');
                 temp_output <= (others => '0');
-                if (PPM_Input = 1) then
+                if (PPM_Input = '0') then
                     validate_counter <= 0;
-                    NS <= VALIDATE;
+                    NS <= VALIDATE_LOW;
                 else 
                     NS <= IDLE;
                 end if;
 
             when VALIDATE_HIGH =>
-                if (PPM_Input = 1) then 
-                    validate_counter = validate_counter + 1;
+                if (PPM_Input = '1') then 
+                    validate_counter <= validate_counter + 1;
                     if (validate_counter >= 3) then
                         NS <= MEASURE;
                     end if;
                 else 
                     if (counter = 0) then
-                        NS <= IDLE;
+                        validate_counter <= 0;
+                        NS <= VALIDATE_LOW;
                     else 
                         validate_counter <= 0;
                         NS <= VALIDATE_HIGH;
@@ -105,23 +107,26 @@ begin
                     temp_output <= temp_output + 1;
                 end if;
                 
-                if(PPM_Input = 0) then
+                if(PPM_Input = '0') then
                     --NS <= GAP;
-                    validate_bit = 0
-                    NS <= VALIDATE_LOW;
+                    NS <= STORE;
                 else 
                     NS <= MEASURE;
-                    validate_counter = 0;
                 end if;
 
             when VALIDATE_LOW =>
-                if (PPM_Input = 0) then 
-                    validate_counter = validate_counter + 1;
+                if (PPM_Input = '0') then 
+                    validate_counter <= validate_counter + 1;
                     if (validate_counter >= 3) then
-                        NS <= STORE;
+                        NS <= VALIDATE_HIGH;
                     end if;
                 else 
-                    NS <= MEASURE;
+                    if (counter = 0) then
+                        NS <= IDLE;
+                    else 
+                        validate_counter <= 0;
+                        NS <= VALIDATE_LOW;
+                    end if;
                 end if;
 
             --when GAP  =>
@@ -145,7 +150,7 @@ begin
                 if (counter = 6) then
                     NS <= IDLE;
                 else 
-                    NS <= MEASURE;
+                    NS <= VALIDATE_HIGH;
                 end if;
 
             when others =>
--- Capture_PPM_tb.vhd
+++ Capture_PPM_tb.vhd
@@ -2,12 +2,13 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use STD.ENV.all;
 
 
 entity Capture_PPM_tb is
 port
 (
-  my_in : in std_logic;
+  my_in : in std_logic
 );
 end Capture_PPM_tb;
 
@@ -28,7 +29,7 @@ end component Capture_PPM;
 
 type state_type is (IDLE,VALIDATE,MEASURE,GAP,STORE);
 
-signal PPM_Input : std_logic;
+signal PPM_Input1 : std_logic;
 
 signal out_channel_1 : STD_LOGIC_VECTOR (31 downto 0);
 signal out_channel_2 : STD_LOGIC_VECTOR (31 downto 0);
@@ -40,14 +41,15 @@ signal out_channel_6 : STD_LOGIC_VECTOR (31 downto 0);
 signal CLK : std_logic;
 signal CLR : std_logic;
 
-signal input_timer : std_logic_vector(11 downto 0);
+signal input_timer : std_logic_vector(31 downto 0);
+signal input_state : state_type;
 
 begin
 
 system_clk_gen : process
 begin 
-    CLK <= '0'
-    wait for 10 ns
+    CLK <= '0';
+    wait for 10 ns;
     loop
         wait for 1 ns;
         CLK <= '1';
@@ -64,70 +66,150 @@ CLR <= '1';
     wait;
 end process toggle_reset;
 
-DUT_stimulus : process(CLK)
+DUT_stimulus : process(CLK,PPM_Input1,CLR,input_timer)
 begin 
     if (CLK = '1' and CLK'event) then
         if (CLR = '1') then
             input_state <= IDLE;
-            PPM_Input <= 0;
-            input_timer <= (others => 0);
+            PPM_Input1 <= '1';
+            input_timer <= (others => '0');
         else
 
-        -- Cycle 1
+        input_timer <= input_timer + 1;
+
+        if input_timer < 5 then
+            PPM_Input1 <= '0';
+        end if ;
+
+        if (input_timer > 5 and input_timer < 50) then
+            PPM_Input1 <= '1';
+        end if ;
+
+        if input_timer > 50 and input_timer < 55 then
+            PPM_Input1 <= '0';
+        end if ;
+
+        if input_timer > 55 and input_timer < 108 then
+            PPM_Input1 <= '1';
+        end if ;
+
+        if input_timer > 108 and input_timer < 113 then
+            PPM_Input1 <= '0';
+        end if ;
+
+        if input_timer > 113 and input_timer < 163 then
+            PPM_Input1 <= '1';
+        end if ;
+
+        if input_timer > 163 and input_timer < 167 then
+            PPM_Input1 <= '0';
+        end if ;
+
+        if input_timer > 167 and input_timer < 217 then
+            PPM_Input1 <= '1';
+        end if ;
+
+        if input_timer > 217 and input_timer < 222 then
+            PPM_Input1 <= '0';
+        end if ;
+
+        if input_timer > 222 and input_timer < 272 then
+            PPM_Input1 <= '1';
+        end if ;
+
+        if input_timer > 272 and input_timer < 276 then
+            PPM_Input1 <= '0';
+        end if ;
+
+        if input_timer > 276 and input_timer < 326 then
+            PPM_Input1 <= '1';
+        end if ;
+
+        if input_timer > 326 and input_timer < 330 then
+            PPM_Input1 <= '0';
+        end if ;
+
+        if input_timer > 330 and input_timer < 350 then
+            PPM_Input1 <= '1';
+        end if ;
+
+
+        -- -- Cycle 1
         
-        PPM_Input <= 1;
-        wait for 1 ms
+        -- PPM_Input1 <= '1';
+        -- --wait for 1 ms;
 
-        PPM_Input <= 0;
-        wait for 403 us
+        -- PPM_Input1 <= '0' after 1 ms;
+        -- --wait for 403 us;
 
-        -- Cycle 2
+        -- -- Cycle 2
         
-        PPM_Input <= 1;
-        wait for 3 ms
+        -- PPM_Input1 <= '1' after 403 us;
+        -- --wait for 3 ms;
 
-        PPM_Input <= 0;
-        wait for 403 us
+        -- PPM_Input1 <= '0' after 3 ms;
+        -- -- wait for 403 us;
 
-        -- Cycle 3
+        -- -- Cycle 3
         
-        PPM_Input <= 1;
-        wait for 500 us
+        -- PPM_Input1 <= '1' after 403 us;
+        -- --wait for 500 us;
 
-        PPM_Input <= 0;
-        wait for 403 us
+        -- PPM_Input1 <= '0' after 500 us;
+        -- --wait for 403 us;
 
-        -- Cycle 4
+        -- -- Cycle 4
         
-        PPM_Input <= 1;
-        wait for 1 ms
+        -- PPM_Input1 <= '1' after 403 us;
+        -- --wait for 1 ms;
 
-        PPM_Input <= 0;
-        wait for 403 us
+        -- PPM_Input1 <= '0' after 1 ms;
+        -- --wait for 403 us;
 
-        -- Cycle 5
+        -- -- Cycle 5
         
-        PPM_Input <= 1;
-        wait for 5 ms
+        -- PPM_Input1 <= '1' after 403 us;
+        -- --wait for 5 ms;
 
-        PPM_Input <= 0;
-        wait for 403 us
+        -- PPM_Input1 <= '0' after 5 ms;
+        -- --wait for 403 us;
 
-        -- Cycle 6
+        -- -- Cycle 6
         
-        PPM_Input <= 1;
-        wait for 7 ms
+        -- PPM_Input1 <= '1' after 403 us;
+        -- --wait for 7 ms;
 
-        PPM_Input <= 0;
-        wait for 403 us
+        -- PPM_Input1 <= '0' after 7 ms;
+        -- --wait for 403 us;
 
-        -- Cycle 7 2(1)
+        -- -- Cycle 7 2(1)
         
-        PPM_Input <= 1;
-        wait for 4 ms
+        -- PPM_Input1 <= '1' after 403 us;
+        -- --wait for 4 ms;
 
-        PPM_Input <= 0;
-        wait for 403 us
+        -- PPM_Input1 <= '0' after 4 ms;
+        -- --wait for 403 us;
+        end if;
+    end if;
+
+    -- finish;
+end process DUT_stimulus;
+
+my_dut : Capture_PPM
+port map
+(
+    PPM_Input => PPM_Input1,
+    CLK => CLK,
+    CLR => CLR, 
+    out_channel_1 => out_channel_1,
+    out_channel_2 => out_channel_2,
+    out_channel_3 => out_channel_3,
+    out_channel_4 => out_channel_4,
+    out_channel_5 => out_channel_5,
+    out_channel_6 => out_channel_6
+);
+
+end rtl;
 
 
 
